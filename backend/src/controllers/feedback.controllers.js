c442c5b028235d151f2b182a02cfb339d62c5627
Rahul Yadav <rahul.yadav6131766@gmail.com>
2024-03-02 13:33:50 +0530

    Added Validations in Controller

--- backend/src/controllers/feedback.controllers.js
+++ backend/src/controllers/feedback.controllers.js
@@ -6,6 +6,7 @@ export const submitFeedback = async (req, res) => {
     const ipAddress = req.ip;
     const {
       name,
+      email,
       company,
       relationship,
       communicationSkills,
@@ -16,10 +17,28 @@ export const submitFeedback = async (req, res) => {
       comments,
     } = req.body;
 
+    // Check for empty fields
+    if (
+      !name ||
+      !email ||
+      !company ||
+      !relationship ||
+      !communicationSkills ||
+      !teamwork ||
+      !leadership ||
+      !adaptability ||
+      !overallSatisfaction
+    ) {
+      return res
+        .status(400)
+        .json({ success: false, error: "All fields are required" });
+    }
+
     // Create a new feedback instance
     const newFeedback = new Feedback({
       ipAddress,
       name,
+      email,
       company,
       relationship,
       communicationSkills,
@@ -30,8 +49,20 @@ export const submitFeedback = async (req, res) => {
       comments,
     });
 
+    // Validate email uniqueness
+    const existingFeedback = await Feedback.findOne({ email });
+    if (existingFeedback) {
+      return res.status(400).json({
+        success: false,
+        error: "Feedback with this email already exist!",
+      });
+    }
+
     // Save the feedback to the database
     const savedFeedback = await newFeedback.save();
+
+    // Set a cookie indicating that the user/feedback is created
+    res.cookie("feedbackCreated", true, { maxAge: 86400000, httpOnly: true });
 
     // Send a JSON response with the saved feedback data
     res.status(201).json({ success: true, feedback: savedFeedback });
--- backend/src/models/feedback.models.js
+++ backend/src/models/feedback.models.js
@@ -13,6 +13,11 @@ const feedbackSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
     company: {
       type: String,
       required: true,
