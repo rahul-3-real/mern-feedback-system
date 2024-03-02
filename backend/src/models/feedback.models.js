86f879a20b7310960250ed8773b298d701deb409
Rahul Yadav <rahul.yadav6131766@gmail.com>
2024-03-02 10:41:56 +0530

    Added Feedback Schema

---
+++ backend/src/models/feedback.models.js
@@ -0,0 +1,80 @@
+import mongoose from "mongoose";
+
+const feedbackSchema = new mongoose.Schema(
+  {
+    // IP Address
+    ipAddress: {
+      type: String,
+      required: true,
+    },
+
+    // Personal Information
+    name: {
+      type: String,
+      required: true,
+    },
+    company: {
+      type: String,
+      required: true,
+    },
+
+    // Position/Relationship Information
+    relationship: {
+      type: {
+        category: {
+          type: String,
+          enum: ["Colleague", "Employer", "Freelance Client", "Other"],
+          required: true,
+        },
+        customText: {
+          type: String,
+        },
+      },
+      required: true,
+    },
+
+    // Rating Scale
+    communicationSkills: {
+      type: Number,
+      min: 1,
+      max: 5,
+      required: true,
+    },
+    teamwork: {
+      type: Number,
+      min: 1,
+      max: 5,
+      required: true,
+    },
+    leadership: {
+      type: Number,
+      min: 1,
+      max: 5,
+      required: true,
+    },
+    adaptability: {
+      type: Number,
+      min: 1,
+      max: 5,
+      required: true,
+    },
+
+    // Overall Satisfaction
+    overallSatisfaction: {
+      type: Number,
+      min: 1,
+      max: 5,
+      required: true,
+    },
+
+    // Comments or Suggestions
+    comments: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+const Feedback = mongoose.model("Feedback", feedbackSchema);
+
+export default Feedback;
